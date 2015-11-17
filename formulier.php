9339cdbbd79ff103b2d94c47fc9ac9e83d6f5ae0
esthervanweenen <117959@WS-M3054.lekenlinge.local>
2015-11-17 09:00:03 +0100

    Formulier en login gemaakt
    Formulier en login gemaakt

---
+++ formulier.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html>
+	<head>
+		<title>Login</title>
+	</head>
+	<body>
+<?php
+if( !isSet( $_SESSION['login'] ) ){
+	$_SESSION['login'] = false;
+	$_SESSION['user'] = 0;
+}
+
+if( $_SESSION['login'] ){
+	echo 'Hallo ' . $_SESSION['user'];
+} else {
+?>
+<form method="POST" action="login.php">
+	<input type="text" name="lln" id="lln" required placeholder="Gebruikersnaam" />
+	<input type="password" name="ww" id="ww" required placeholder="Wachtwoord" />
+	<input type="submit" value="Login" />
+</form>
+<?php
+}
+?>
+	</body>
+</html>
+
+
